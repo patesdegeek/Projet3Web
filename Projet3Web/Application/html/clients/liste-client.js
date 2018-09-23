1636c5759c79495f8e26777dbd9be9a0a8acf9ea
lauparr <laurent.parrot78@gmail.com>
2018-09-23 15:35:21 +0200

    Ajout de la page pour lister les clients + controlleur web api

---
+++ Projet3Web/Application/html/clients/liste-client.js
@@ -0,0 +1,26 @@
+﻿function getClients() {
+    var compTableBody = $("#table-body");
+    $.ajax({
+        url: "/api/clients",
+        success: function (clients) {
+            compTableBody.empty();
+
+            if (clients.length > 0) {
+                for (client of clients) {
+                    var ligneClient = $("<tr></tr>");
+                    var ligneClient1 = $("<td>" + client.Id + "</td>");
+                    var ligneClient2 = $("<td>" + client.Nom + "</td>");
+                    var ligneClient3 = $("<td>" + client.Prenom + "</td>");
+                    var ligneClient4 = $("<td>" + client.Email + "</td>");
+                    ligneClient.append(ligneClient1);
+                    ligneClient.append(ligneClient2);
+                    ligneClient.append(ligneClient3);
+                    ligneClient.append(ligneClient4);
+                    compTableBody.append(ligneClient);
+                }
+            }
+        }
+    })
+}
+
+$(document).ready(getClients);
---
+++ Projet3Web/Controllers/ClientsController.cs
@@ -0,0 +1,22 @@
+﻿using Projet3Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Projet3Web.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetClients()
+        {
+            return Ok(new Client[] {
+                new Client {Id=1, Civilite = "Mr", Nom = "Parrot", Prenom = "Laurent", Email = "kestounet@gmail.com", Adresse = "11, Caserne des Gardes 78120 Rambouillet", DateNaissance = DateTime.Parse("05/09/1983"), Telephone = "0648091132"},
+                new Client {Id=2, Civilite = "Mme", Nom = "Parrot", Prenom = "Anne", Email = "patesdegeek@gmail.com", Adresse = "11, Caserne des Gardes 78120 Rambouillet", DateNaissance = DateTime.Parse("13/07/1982"), Telephone = "0123456789"}
+            });
+        }
+    }
+}
