f1f3c91cef40467b7824b60eec2264b68ab08875
patesdegeek <patesdegeek@gmail.com>
2018-09-23 15:31:23 +0200

    Ajout fichier Data, classe ApplicationDbContext, et ajout connection string

---
+++ Projet3Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Projet3Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Projet3Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("ApplicationConnectionString")
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
